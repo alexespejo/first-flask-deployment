01828f2e6625d159fb576fcd554f0dff74396773
Alex Espejo <aespejo102@gmail.com>
2023-12-31 19:25:54 -0800

    testing an api

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import requests 
+from bs4 import BeautifulSoup 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return {"hello" : "test"}
+    URL = "http://www.values.com/inspirational-quotes"
+    r = requests.get(URL) 
+    
+    soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
+    return soup.prettify()
